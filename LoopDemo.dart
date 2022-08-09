8518a6b0977d5c99d2ea21bde803a00e058266d9
kelviBarochiya <kelvibarochiya@gmail.com>
2022-08-09 12:36:16 +0530

    Loop Demo

---
+++ LoopDemo.dart
@@ -0,0 +1,12 @@
+void main() {
+  int a = 8;
+  int b = 12;
+  int c = a + b;
+  if (c == 20) {
+      print("C is 20");
+    } else if (c < 20) {
+      print("C is  < 20");
+    } else {
+      print("C is > 20");
+    }
+}
