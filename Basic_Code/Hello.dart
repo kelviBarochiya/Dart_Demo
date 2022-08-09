12b4cf93a5efc0bab4fe9a87ed9bb33e3add7c58
kelviBarochiya <108574688+kelviBarochiya@users.noreply.github.com>
2022-08-09 20:21:34 +0530

    Basic

---
+++ Basic_Code/Hello.dart
@@ -0,0 +1,9 @@
+void main() {
+  print("Welcome to Dart!");
+  int a = 8;
+  int b = 12;
+  int c = a + b;
+  print("A value is ${a} B value is ${b}");
+  print("Sum of A and B is ${c} ");
+  print("Just for Testing Purpose");
+}
